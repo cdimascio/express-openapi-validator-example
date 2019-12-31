078f33db651f909fd800c887823740149dff040a
Carmine DiMascio <cdimascio@tripadvisor.com>
2019-12-31 15:38:12 -0500

    update example to use latest validator

--- app.js
+++ app.js
@@ -22,63 +22,64 @@ app.use('/spec', express.static(spec));
 // 1. Install the OpenApiValidator on your express app
 new OpenApiValidator({
   apiSpec: './openapi.yaml',
-  // securityHandlers: {
-  //   ApiKeyAuth: (req, scopes, schema) => true,
-  // },
-}).install(app);
+})
+  .install(app)
+  .then(() => {
+    // 2. Add routes
+    app.get('/v1/pets', function(req, res, next) {
+      res.json([
+        { id: 1, name: 'max' },
+        { id: 2, name: 'mini' },
+      ]);
+    });
 
-// 2. Add routes
-app.get('/v1/pets', function(req, res, next) {
-  res.json([{ id: 1, name: 'max' }, { id: 2, name: 'mini' }]);
-});
+    app.post('/v1/pets', function(req, res, next) {
+      res.json({ name: 'sparky' });
+    });
 
-app.post('/v1/pets', function(req, res, next) {
-  res.json({ name: 'sparky' });
-});
+    app.get('/v1/pets/:id', function(req, res, next) {
+      res.json({ id: req.params.id, name: 'sparky' });
+    });
 
-app.get('/v1/pets/:id', function(req, res, next) {
-  res.json({ id: req.params.id, name: 'sparky' });
-});
+    // 2a. Add a route upload file(s)
+    app.post('/v1/pets/:id/photos', function(req, res, next) {
+      // DO something with the file
+      // files are found in req.files
+      // non file multipar params are in req.body['my-param']
+      console.log(req.files);
 
-// 2a. Add a route upload file(s)
-app.post('/v1/pets/:id/photos', function(req, res, next) {
-  // DO something with the file
-  // files are found in req.files
-  // non file multipar params are in req.body['my-param']
-  console.log(req.files);
+      res.json({
+        files_metadata: req.files.map(f => ({
+          originalname: f.originalname,
+          encoding: f.encoding,
+          mimetype: f.mimetype,
+          // Buffer of file conents
+          // buffer: f.buffer,
+        })),
+      });
+    });
 
-  res.json({
-    files_metadata: req.files.map(f => ({
-      originalname: f.originalname,
-      encoding: f.encoding,
-      mimetype: f.mimetype,
-      // Buffer of file conents
-      // buffer: f.buffer,
-    })),
-  });
-});
-
-// 3. Create a custom error handler
-app.use((err, req, res, next) => {
-  // format error
-  if (!err.status && !err.errors) {
-    res.status(500).json({
-      errors: [
-        {
+    // 3. Create a custom error handler
+    app.use((err, req, res, next) => {
+      // format error
+      if (!err.status && !err.errors) {
+        res.status(500).json({
+          errors: [
+            {
+              message: err.message,
+            },
+          ],
+        });
+      } else {
+        res.status(err.status).json({
           message: err.message,
-        },
-      ],
+          errors: err.errors,
+        });
+      }
     });
-  } else {
-    res.status(err.status).json({
-      message: err.message,
-      errors: err.errors,
-    });
-  }
-});
 
-const server = http.createServer(app);
-server.listen(3000);
-console.log('Listening on port 3000');
-
+    const server = http.createServer(app);
+    server.listen(3000);
+    console.log('Listening on port 3000');
+  });
 module.exports = app;
